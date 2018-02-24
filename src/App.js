974225fd0361c6017104a1cadb72b40885194877
Waqar Ahmed <waqar.nu@gmail.com>
2018-02-24 12:47:35 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import './App.css';
+
+
+
+const particlesOptions = {
+  particles: {
+   number: {
+    value: 100,
+    density: {
+      enable: true,
+      value_area: 500
+    }
+   }
+   }
+   } 
+
+const initailState = {
+
+
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+
+    }
+
+class App extends Component {
+
+  constructor() {
+
+    super();
+    this.state = initailState;
+  }
+
+  loadUser = (data) => {
+
+    this.setState({user: {
+
+       id: data.id,
+       name: data.name,
+       email: data.email,
+       entries: data.entries,
+       joined: data.joined
+
+    }})
+
+
+  }
+
+
+  calculateFaceLocation = (data) => {
+
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    //console.log(width, height);
+
+    return {
+
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+
+ }
+
+ displayFaceBox = (box) => {
+
+  //console.log(box);
+  this.setState({box: box});
+
+
+ }
+
+  onInputChange = (event) => {
+
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+
+    this.setState({imageUrl: this.state.input});
+
+    //console.log('click');
+     fetch('http://localhost:3001/imageurl', {
+            method: 'post',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+            input: this.state.input
+      })
+          })
+      .then (response => response.json())
+
+    
+      .then(response => {
+
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+            id: this.state.user.id
+      })
+          }).then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+
+    if (route === 'signout') {
+
+      this.setState(initailState)
+    } else if (route === 'home') {
+
+      this.setState({isSignedIn: true})
+
+  }
+      this.setState({route: route});
+  }
+
+  render() {
+
+    const {isSignedIn, imageUrl, route, box} = this.state;
+    
+    return (
+        <div className="App">
+           <Particles className='particles' 
+                params={particlesOptions } />
+
+        <Navigation isSignedIn ={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' ?
+
+        <div>
+          <Logo />
+          <Rank name={this.state.user.name} entries={this.state.user.entries} />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          : (
+            route === 'signin' ?  
+            <SignIn loadUser={this.loadUser} onRouteChange = {this.onRouteChange} />
+            :
+            <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
+
+
+            )
+
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import circuit from './circuit.png'
+import './Logo.css';
+
+const Logo = () => {
+
+	return (
+
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+ 				<div className="Tilt-inner"><img style={{paddingTop:'15px'}} alt= 'Logo' src={circuit}/>
+ 				</div>
+			</Tilt>
+		</div>
+
+		);
+}
+
+export default Logo;
